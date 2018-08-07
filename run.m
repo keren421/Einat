5892678c2082c8841de30739e455139dfbdb1e8e
Keren421 <kerenbenzvi@gmail.com>
2018-08-07 16:57:59 +0300

    changed mutation chance
    changed mutation chance
    added a graph to show the improvement in each time step

--- run.m
+++ run.m
@@ -3,15 +3,15 @@ switch 1
     case 1
         N = 2 ; % number of species
         K = 2 ; % number of antibiotics
-        Cost = [0.05 0.5] ; % resistance and production costs
+        Cost = [0.5 0.5] ; % resistance and production costs
         Mut_prod = 0.5; % chance of a mutation affecting production 1-Pprod chance of affecting resistance ;
-        Mut_size = [0 0]; % average size of resistant and production mutations (typically should be <=0)
+        Mut_size = [-0.1 -0.1]; % average size of resistant and production mutations (typically should be <=0)
         Mut_size_std = [0.05 0.05]; % standard deviation of resistant and production mutations
         Mut_0 = [0 0] ; % chance of null mutations causing complete loss of resistant(1) or production(2) 
     case 2
         N = 8 ; 
         K = 4 ; 
-        Cost = [0.05 0.5] ; 
+        Cost = [0.5 0.5] ; 
         Mut_prod = 0.5; 
         Mut_size = [0 0]; 
         Mut_size_std = [0.05 0.05]; 
@@ -22,8 +22,9 @@ Phen = zeros(K,2,N) ; %1:Res, 2:Production
 t = 0 ; % number of cycles
 it = 0 ; % number of fixation events
 maxit = 1000; %1000 ; % max number of fixations 
-t_v = zeros(maxit,1) ;
-Phen_v = zeros(K,2,N,maxit) ; % keeps all phenotypes versus time
+t_v = nan(maxit,1) ;
+improvement = nan(maxit,1) ;
+Phen_v = nan(K,2,N,maxit) ; % keeps all phenotypes versus time
 
 %% run
 while it<maxit
@@ -55,20 +56,22 @@ while it<maxit
         end
         
         % fixation
-        if rand<fMT/fWT-1
+        if rand<0.5*(1+tanh((fMT/fWT-6)))
             it = it + 1 ;
             Phen(:,:,n) = MT ;
             Phen_v(:,:,:,it) = Phen ;
             t_v(it,1) = t ;
+            improvement(it,1) = fMT/fWT ;
+            
         end
     end
     t = t + 1 ;
     if ~mod(t,100000), disp([t,it]); end
 end
 %% plot
-figure(4);clf;
+figure(2);clf;
 for k = 1:K
-    subplot(K,1,k)
+    subplot(K+1,1,k)
     hold on
     plot(t_v,reshape(Phen_v(k,1,:,:),N,[]),'-');
     ax = gca;
@@ -76,13 +79,15 @@ for k = 1:K
     plot(t_v,reshape(Phen_v(k,2,:,:),N,[]),'--')
     title(sprintf('-Resistant, --Production, Antibiotic #%g',k))
 end
+subplot(k+1,1,k+1); hold on
+plot(t_v,improvement,'-');
 
 %%
 function [y1,y2] = single_droplet(P1,P2,cost)
 f1 = fitness(P1,cost) ;
 f2 = fitness(P2,cost) ;
 
-switch 'roi'
+switch 'keren'
     case 'roi'
         i1 = all(P1(:,1)>=P2(:,2)) ;
         i2 = all(P2(:,1)>=P1(:,2)) ;
@@ -99,7 +104,7 @@ switch 'roi'
         growth1 = f1 - min(sum(max(P2(:,2)+P1(:,2)-P1(:,1),0)),f1);
         growth2 = f2 - min(sum(max(P1(:,2)+P2(:,2)-P2(:,1),0)),f2);
         y1 = growth1/(growth1+growth2);
-        y2 = growth2/(growth1+growth2) ;
+        y2 = growth2/(growth1+growth2);
 end
 end
 
