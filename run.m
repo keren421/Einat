42bf6920053bd554d6a79c46f8e67f75f29afd5b
Keren421 <kerenbenzvi@gmail.com>
2018-08-14 10:27:10 +0300

    seperated droplet

--- run.m
+++ run.m
@@ -1,14 +1,16 @@
 %% setup
 switch 1
     case 1
+        start_rand = false;
         N = 2 ; % number of species
-        K = 2 ; % number of antibiotics
+        K = 1 ; % number of antibiotics
         Cost = [0.5 0.5] ; % resistance and production costs
         Mut_prod = 0.5; % chance of a mutation affecting production 1-Pprod chance of affecting resistance ;
         Mut_size = [-0.05 -0.05]; % average size of resistant and production mutations (typically should be <=0)
         Mut_size_std = [0.05 0.05]; % standard deviation of resistant and production mutations
         Mut_0 = [0 0] ; % chance of null mutations causing complete loss of resistant(1) or production(2) 
     case 2
+        start_rand = false;
         N = 8 ; 
         K = 4 ; 
         Cost = [0.5 0.5] ; 
@@ -18,17 +20,26 @@ switch 1
         Mut_0 = [0 0] ; 
 end
 
-Phen = zeros(K,2,N) ; %1:Res, 2:Production
+%%
+if start_rand
+    Phen = rand(k,2,N);
+else
+    Phen = zeros(K,2,N) ; %1:Res, 2:Production
+end
+
 t = 0 ; % number of cycles
 it = 0 ; % number of fixation events
 maxit = 1000; %1000 ; % max number of fixations 
 t_v = nan(maxit,1) ;
-improvement = nan(maxit,1) ;
+improvement = nan(maxit,1) ; %saves how beneficial was the mutation
 Phen_v = nan(K,2,N,maxit) ; % keeps all phenotypes versus time
+max_rounds = 1e8;
+i_round = 0; 
 
 %% run
-while it<maxit
-    for n = 1:N 
+while (it<maxit)&&(i_round<max_rounds)
+    i_round = i_round + 1;
+    for n = randperm(N) 
         WT = Phen(:,:,n) ;
         MT = WT ;
         k = randi(K) ; % chose number of antibiotic to mutate
@@ -40,10 +51,10 @@ while it<maxit
         P0 = P0 * (rand>Mut_0(p));
         MT(k,p) = max(P0,0) ;
         
-        if MT(k,1)<MT(k,2)
-            %MT(k,2) = MT(k,1);
-            MT = WT ;
-        end
+%         if MT(k,1)<MT(k,2)
+%             %MT(k,2) = MT(k,1);
+%             MT = WT ;
+%         end
         
         % calc average fitness
         fWT = 0 ;
@@ -56,20 +67,26 @@ while it<maxit
         end
         
         % fixation
-        if rand<0.5*(1+tanh((fMT/fWT-6)))
+        switch 'only_beneficial'
+            case 'only_beneficial'
+                thereshold = fMT/fWT - 1;
+            case 'allow_negative'
+                x = fMT/fWT;
+                thereshold = 0.5*(1+tanh(10*(x-1.15)));
+        end
+        if rand < thereshold
             it = it + 1 ;
             Phen(:,:,n) = MT ;
             Phen_v(:,:,:,it) = Phen ;
             t_v(it,1) = t ;
             improvement(it,1) = fMT/fWT ;
-            
         end
     end
     t = t + 1 ;
     if ~mod(t,100000), disp([t,it]); end
 end
 %% plot
-figure(2);clf;
+figure(3);clf;
 for k = 1:K
     subplot(K+1,1,k)
     hold on
@@ -81,36 +98,4 @@ for k = 1:K
 end
 subplot(k+1,1,k+1); hold on
 plot(t_v,improvement,'-');
-
-%%
-function [y1,y2] = single_droplet(P1,P2,cost)
-f1 = fitness(P1,cost) ;
-f2 = fitness(P2,cost) ;
-
-switch 'keren'
-    case 'roi'
-        i1 = all(P1(:,1)>=P2(:,2)) ;
-        i2 = all(P2(:,1)>=P1(:,2)) ;
-        res = f1+f2 ;
-        %why divide by f1+f2?
-        y1 = i1/(i1+i2)*f1 ;
-        y2 = i2/(i1+i2)*f2 ;
-    case 'keren'
-        growth1 = f1 - min(sum(max(P2(:,2)-P1(:,1),0)),f1);
-        growth2 = f2 - min(sum(max(P1(:,2)-P2(:,1),0)),f2);
-        y1 = growth1/(growth1+growth2);
-        y2 = growth2/(growth1+growth2) ;
-    case 'keren_2'
-        growth1 = f1 - min(sum(max(P2(:,2)+P1(:,2)-P1(:,1),0)),f1);
-        growth2 = f2 - min(sum(max(P1(:,2)+P2(:,2)-P2(:,1),0)),f2);
-        y1 = growth1/(growth1+growth2);
-        y2 = growth2/(growth1+growth2);
-end
-end
-
-
-function f = fitness(P,cost)
-%any reason to do it with exp or just random choice?
-f = exp(-sum(P(:,1)*cost(1) + P(:,2)*cost(2))) ;
-end
 
---
+++ single_droplet.m
@@ -0,0 +1,30 @@
+function [y1,y2] = single_droplet(P1,P2,cost)
+f1 = fitness(P1,cost) ;
+f2 = fitness(P2,cost) ;
+
+switch 'roi'
+    case 'roi'
+        i1 = all(P1(:,1)>=P2(:,2)) ;
+        i2 = all(P2(:,1)>=P1(:,2)) ;
+        res = f1+f2 ;
+        %why divide by f1+f2?
+        y1 = i1/(i1+i2)*f1 ;
+        y2 = i2/(i1+i2)*f2 ;
+    case 'keren'
+        growth1 = f1 - min(sum(max(P2(:,2)-P1(:,1),0)),f1);
+        growth2 = f2 - min(sum(max(P1(:,2)-P2(:,1),0)),f2);
+        y1 = growth1/(growth1+growth2);
+        y2 = growth2/(growth1+growth2) ;
+    case 'keren_2'
+        growth1 = f1 - min(sum(max(P2(:,2)+P1(:,2)-P1(:,1),0)),f1);
+        growth2 = f2 - min(sum(max(P1(:,2)+P2(:,2)-P2(:,1),0)),f2);
+        y1 = growth1/(growth1+growth2);
+        y2 = growth2/(growth1+growth2);
+end
+end
+
+
+function f = fitness(P,cost)
+%any reason to do it with exp or just random choice?
+f = exp(-sum(P(:,1)*cost(1) + P(:,2)*cost(2))) ;
+end
