ab15f8dd8f2ff9f15f5e7ef691355674f28c37f2
keren421 <kerenbenzvi@gmail.com>
2018-10-16 16:35:14 +0300

    fixed plotPhen

--- run.m
+++ run.m
@@ -1,5 +1,5 @@
 %% setup
-scoring_type = 'winner_gets_all'; % 'winner_gets_all', 'loser_dies_winner_gets_rest', 'loser_remains_winner_gets_rest'
+scoring_type = 'loser_remains_winner_gets_rest'; % 'winner_gets_all', 'loser_dies_winner_gets_rest', 'loser_remains_winner_gets_rest'
 type_resist = 'max'; % max , plus, resist
 decay = 1;
 start_rand = false;
@@ -9,14 +9,14 @@ Mut_size = [-0.2 -0.2]; % average size of resistant and production mutations (ty
 Mut_size_std = [0.2 0.2]; % standard deviation of resistant and production mutations
 Mut_0 = [0.1 0.1] ; % chance of null mutations causing complete loss of resistant(1) or production(2) 
 maxit = 1000; %1000 ; % max number of fixations 
-switch 1
+switch 2
     case 1
-        fig_num = 110;
+        fig_num = 100;
         N = 2 ; % number of species
         K = 2 ; % number of antibiotics        
         maxit = 1000; % max number of fixations 
     case 2
-        fig_num = 100;
+        fig_num = 110;
         N = 8 ; % number of species
         K = 4 ; % number of antibiotics
         maxit = 4000;% max number of fixations 
