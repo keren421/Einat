9ebcb8701bb79123bc329a32583ce5fe89ba3dd2
keren421 <kerenbenzvi@gmail.com>
2018-11-06 14:08:13 +0200

    added self competition

--- mean_production.m
+++ mean_production.m
@@ -4,6 +4,7 @@ dataObjs = get(axesObjs, 'Children');
 lines = dataObjs{2};
 
 average_production = nan(length(lines),1);
+var_production = nan(length(lines),1);
 run_type = nan(length(lines),1);
 
 for i = 1:length(lines)
@@ -12,6 +13,7 @@ for i = 1:length(lines)
     temp_p = temp_p(~isnan(temp_p));
     temp_t = temp_t(~isnan(temp_p));
     average_production(i) = trapz(temp_t,temp_p)/max(temp_t);
+    %var_production(i) =  trapz(temp_t,temp_p)/max(temp_t);
     run_type(i) = str2double(lines(i).DisplayName);
 end
 
