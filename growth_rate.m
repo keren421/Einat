72d09f44883e698864234783a7bd118b28b9926e
keren421 <kerenbenzvi@gmail.com>
2018-12-18 16:23:43 +0200

    organized folder

--- growth_rate.m
+++ growth_rate.m
@@ -1,5 +1,3 @@
 function g = growth_rate(P,cost)
-%any reason to do it with exp or just random choice?
 g = exp(-sum(P(:,1)*cost(1) + P(:,2)*cost(2))) ;
-%g = 1-(P(:,2)>0)*0.3-(P(:,1)>0)*0.3 - sum(P(:,1)*cost(1) + P(:,2)*cost(2)) ;
 end
